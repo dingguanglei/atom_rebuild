1ab1094d4d0960123c56f4ace31c5c603de07245
dingguanglei <32232377+dingguanglei@users.noreply.github.com>
2018-08-04 16:05:25 +0800

    JDIT

--- pt_wgangp_train.py
+++ pt_wgangp_train.py
@@ -3,20 +3,16 @@ import time
 import os
 import torch
 import copy
-
 from torch.autograd import Variable
 from torch.optim import Adam, RMSprop
-
-import torchvision.transforms as transforms
-from torchvision.utils import make_grid
-from tensorboardX import SummaryWriter
 
 from mypackage.data import IMAGE_PATH, MASK_PATH_DIC, getDataLoader
 from mypackage.utils import ElapsedTimer, checkPoint, Watcher, buildDir
 from mypackage.tricks import gradPenalty, spgradPenalty, jcbClamp, getPsnr
 from mypackage.model.define import defineNet
 from mypackage.model.unet import Unet_G
-from mypackage.model.wnet import NLayer_D, Wnet_G
+from mypackage.model.wnet import NLayer_D, Wnet_G,Attn_Discriminator,Attn_Wnet_G
+from mypackage.optimizer import Optimizer
 
 
 class Trainer(object):
@@ -29,20 +25,23 @@ class Trainer(object):
         self.netD = netD
         self.count = 0
         self.steps = len(train_loader)
-        # self.train_losses_dic = {'G': [], 'D': [], 'GP': [], "SGP": [], 'WD': [], 'GN': [], 'JC': [], 'PSNR': []}
-        # self.valid_losses_dic = {'G': [], 'D': [], 'WD': [], 'JC': [], 'PSNR': []}
         self.watcher = Watcher(logdir="log")
 
         self.lr = 1e-3
-        self.lr_decay = 0.94
+        self.lr_decay = 0.8
         self.weight_decay = 2e-5
-        self.opt_g = Adam(self.netG.parameters(), lr=self.lr, betas=(0.9, 0.99), weight_decay=self.weight_decay)
-        self.opt_d = RMSprop(self.netD.parameters(), lr=self.lr, weight_decay=self.weight_decay)
+        self.betas = (0.9, 0.99)
+        self.opt_g = Optimizer(filter(lambda p: p.requires_grad, self.netG.parameters()), self.lr, self.lr_decay, self.weight_decay, betas=self.betas,
+                               opt_name="Adam")
+        # self.opt_g = Adam(self.netG.parameters(), lr=self.lr, betas=(0.9, 0.999), weight_decay=self.weight_decay)
+        self.opt_d = Optimizer(filter(lambda p: p.requires_grad, self.netD.parameters()), self.lr, self.lr_decay,
+                               self.weight_decay, opt_name="RMSprop")
+        # self.opt_ds = RMSprop(filter(lambda p: p.requires_grad, self.netD.parameters()), lr=self.lr,
+        #                      weight_decay=self.weight_decay)
+        self.nepochs = 150 * 5
 
-        self.nepochs = 500
-
-        # net_cpu = copy.deepcopy(self.netG).cpu()
-        # self.watcher.graph(net_cpu, input_shape=(2, 1, 256, 256))
+        net_cpu = copy.deepcopy(self.netG).cpu()
+        self.watcher.graph(net_cpu, input_shape=(2, 1, 256, 256))
 
     def _dis_train_iteration(self, input, fake, real):
         self.opt_d.zero_grad()
@@ -51,42 +50,33 @@ class Trainer(object):
         d_real = self.netD(real, input)
 
         gp = gradPenalty(self.netD, real, fake, input=input, use_gpu=self.use_gpus)
-        sgp = spgradPenalty(self.netD, input, real, fake, type="G", use_gpu=self.use_gpus) * 0.5 + \
-              spgradPenalty(self.netD, input, real, fake, type="X", use_gpu=self.use_gpus) * 0.5
+        sgp = spgradPenalty(self.netD, input, real, fake, type="G", use_gpu=self.use_gpus) * 0.5\
+              + spgradPenalty(self.netD, input, real, fake, type="X", use_gpu=self.use_gpus) * 0.5
         loss_d = d_fake.mean() - d_real.mean() + gp + sgp
         w_distance = (d_real.mean() - d_fake.mean()).detach()
         loss_d.backward()
         self.opt_d.step()
-        #
-        # self.train_losses_dic["D"].append(loss_d.detach())
-        # self.train_losses_dic["WD"].append(w_distance.detach())
-        # self.train_losses_dic["GP"].append(gp.detach())
-        # self.train_losses_dic["SGP"].append(sgp.detach())
-        # self.watcher.scalars(["D", "GP", "WD", "SGP"], self.train_losses_dic, self.count, tag="Train")
         self.watcher.scalars(["D", "GP", "WD", "SGP"], [loss_d, gp, w_distance, sgp], self.count, tag="Train")
         d_log = "Loss_D: {:.4f}".format(loss_d.detach())
         return d_log
 
-    def _gen_train_iteration(self, input):
+    def _gen_train_iteration(self, input, fake=None):
         self.opt_g.zero_grad()
-
-        fake = self.netG(input)
+        if fake is None:
+            fake = self.netG(input)
         d_fake = self.netD(fake, input)
-        jc = jcbClamp(self.netG, input, use_gpu=self.use_gpus)
-        loss_g = -d_fake.mean() + jc
+        # jc = jcbClamp(self.netG, input, use_gpu=self.use_gpus)
+        loss_g = -d_fake.mean()
         loss_g.backward()
         self.opt_g.step()
         psnr = getPsnr(fake, input, self.use_gpus)
-        # self.train_losses_dic["PSNR"].append(psnr)
-        # self.train_losses_dic["JC"].append(jc.detach())
-        # self.train_losses_dic["G"].append(loss_g.detach())
-        # self.watcher.scalars(["PSNR", "JC", "G"], self.train_losses_dic, self.count, tag="Train")
-        self.watcher.scalars(["PSNR", "JC", "G"], [psnr, jc, loss_g], self.count, tag="Train")
+        self.watcher.scalars(["PSNR", "G"], [psnr, loss_g], self.count, tag="Train")
         g_log = "Loss_G: {:.4f} PSNR:{:.4f}".format(loss_g.detach(), psnr)
         return g_log
 
     def _train_epoch(self, input, real):
         epoch = self.epoch
+        d_turn = 5
         for iteration, batch in enumerate(self.train_loader, 1):
             timer = ElapsedTimer()
             self.count += 1
@@ -97,18 +87,25 @@ class Trainer(object):
             fake = self.netG(input)
 
             d_log = self._dis_train_iteration(input, fake.detach(), real)
-            g_log = self._gen_train_iteration(input)
+            g_log = ""
+            if (self.count <= 75 and self.count  % 25 == 0) or (self.count > 75 and self.count % d_turn == 0):
+                g_log = self._gen_train_iteration(input, fake)
+
+            # d_log = self._dis_train_iteration(input, fake.detach(), real)
+            # g_log = self._gen_train_iteration(input, fake)
 
             print("===> Epoch[{}]({}/{}): {}\t{} ".format(
                 epoch, iteration, self.steps, d_log, g_log))
+
             one_step_cost = time.time() - timer.start_time
             left_time_one_epoch = timer.elapsed((self.steps - iteration) * one_step_cost)
             print("leftTime: %s" % left_time_one_epoch)
-
             if iteration == 1:
                 self.watcher.images([input, fake, real], ["input", "fake", "real"], self.epoch, tag="Train",
                                     show_imgs_num=3,
-                                    mode="L")
+                                    mode="L",
+                                    mean= -1,
+                                    std = 2)
 
     def train(self):
         input = Variable()
@@ -126,10 +123,22 @@ class Trainer(object):
             # self.watcher.netParams(self.netG, epoch)
             left_time = timer.elapsed((self.nepochs - epoch) * (time.time() - timer.start_time))
             print("leftTime: %s" % left_time)
-            if epoch % 5 == 0:
+            # if epoch == 1:
+            #     self.opt_g.do_lr_decay(self.netG.parameters(), reset_lr=1e-4)
+            #     self.opt_d.do_lr_decay(filter(lambda p: p.requires_grad, self.netD.parameters()), reset_lr=1e-3)
+            # elif epoch == 40:
+            #     self.opt_g.do_lr_decay(self.netG.parameters(), reset_lr=1e-5)
+            #     self.opt_d.do_lr_decay(filter(lambda p: p.requires_grad, self.netD.parameters()), reset_lr=1e-4)
+            # elif epoch == 80:
+            #     self.opt_g.do_lr_decay(self.netG.parameters(), reset_lr=1e-6)
+            #     self.opt_d.do_lr_decay(filter(lambda p: p.requires_grad, self.netD.parameters()), reset_lr=1e-5)
+            if epoch % 10 == 0:
                 self.lr = self.lr * self.lr_decay
-                self.opt_g = Adam(net_G.parameters(), lr=self.lr, betas=(0.9, 0.99), weight_decay=self.weight_decay)
-                self.opt_d = RMSprop(net_D.parameters(), lr=self.lr, weight_decay=self.weight_decay)
+                self.opt_g.do_lr_decay(filter(lambda p: p.requires_grad, self.netG.parameters()))
+                self.opt_d.do_lr_decay(filter(lambda p: p.requires_grad, self.netD.parameters()))
+                # self.opt_g = Adam(net_G.parameters(), lr=self.lr, betas=(0.9, 0.99), weight_decay=self.weight_decay)
+                # self.opt_d = RMSprop(filter(lambda p: p.requires_grad, self.netD.parameters()), lr=self.lr,
+                #                      weight_decay=self.weight_decay)
                 print("change learning rate to %s" % self.lr)
             if epoch % 10 == 0:
                 self.predict()
@@ -148,7 +157,9 @@ class Trainer(object):
             self.netG.zero_grad()
             self.watcher.images([input, fake, real], ["input", "fake", "real"], self.epoch, tag="Test",
                                 show_imgs_num=8,
-                                mode="L")
+                                mode="L",
+                                mean= -1,
+                                std = 2)
         self.netG.train()
 
     def valid(self):
@@ -191,7 +202,8 @@ class Trainer(object):
         avg_psnr = avg_psnr / len_test_data
         self.watcher.scalars(["D", "G", "WD", "PSNR"], [avg_loss_d, avg_loss_g, avg_w_distance, avg_psnr], self.count,
                              tag="Valid")
-        self.watcher.images([input, fake, real], ["input", "fake", "real"], self.epoch, tag="Valid")
+        self.watcher.images([input, fake, real], ["input", "fake", "real"], self.epoch, tag="Valid",mean= -1,
+                                    std = 2)
         self.netG.train()
         self.netD.train()
         self.netG.zero_grad()
@@ -199,14 +211,14 @@ class Trainer(object):
 
 
 if __name__ == '__main__':
-    os.environ["CUDA_VISIBLE_DEVICES"] = "0,1,2,3"
+    os.environ["CUDA_VISIBLE_DEVICES"] = "2,3"
     print('===> Check directories')
     buildDir()
 
     gpus = (0, 1)
-    d_depth = 2
-    g_depth = 2
-    batchSize = 2
+    d_depth = 32
+    g_depth = 32
+    batchSize = 32
     test_size = 500
     train_size = None
     cv_size = 500
@@ -223,12 +235,17 @@ if __name__ == '__main__':
         valid_size=cv_size)
 
     print('===> Building model')
-    net_G = defineNet(Unet_G(depth=g_depth, norm_type="switch"),
-                      gpu_ids=gpus, use_weights_init=True)
+    # net_G = defineNet(Wnet_G(depth=g_depth, norm_type="instance",active_type="LeakyReLU"),
+    #                   gpu_ids=gpus, use_weights_init=True)
     # net_G = defineNet(Wnet_G(depth=g_depth, active_type="LeakyReLU", norm_type="switch"),
     #                   gpu_ids=gpus, use_weights_init=True)
-    net_D = defineNet(NLayer_D(depth=d_depth, norm_type="instance", use_sigmoid=False, use_liner=False),
-                      gpu_ids=gpus, use_weights_init=True)
+    # net_D = defineNet(NLayer_D(depth=d_depth, norm_type="instance", use_sigmoid=False, use_liner=False),
+    #                   gpu_ids=gpus, use_weights_init=True)
+    net_G = defineNet(Attn_Wnet_G(depth=g_depth, norm_type="batch", active_type="ReLU"),
+                                        gpu_ids=gpus, use_weights_init=True)
+
+    net_D = defineNet(Attn_Discriminator(depth=d_depth),
+                                        gpu_ids=gpus, use_weights_init=True)
 
     print('===> Training')
     Trainer = Trainer(net_G, net_D, trainLoader, testLoader, cvLoader, gpus)
--- read.py
+++ read.py
@@ -9,7 +9,6 @@ from skimage import data
 from torch.autograd import Variable
 from mypackage.utils import Model, buildDir
 from mypackage.data import TestDataset
-
 from tensorboardX import SummaryWriter
 
 if __name__ == '__main__':
@@ -20,37 +19,46 @@ if __name__ == '__main__':
     parser.add_argument('--test_dir', '-td', type=str, help='path of test directory', default="test")
     parser.add_argument('--result_dir', '-rd', type=str, help='path of result directory', default="result")
     parser.add_argument('--model_name', '-mn', type=str, help='model name', default="")
-    parser.add_argument('--min_size', '-ms', type=str, help='model name', default=32*4)
+    parser.add_argument('--min_size', '-ms', type=str, help='model name', default=32 * 4)
+    parser.add_argument('--mean', type=float, help='model name', default=-1)
+    parser.add_argument('--std', type=float, help='model name', default=2)
     args = parser.parse_args()
 
     epoch = args.epoch
-    gpus = [0]
+    gpus = []
     test_dir = args.test_dir
     result_dir = args.result_dir
     name = args.model_name
     min_size = args.min_size
+    mean = args.mean
+    std = args.std
     buildDir([result_dir])
-    testDataset = DataLoader(TestDataset(test_dir, min_size=min_size ), batch_size=1)
+    testDataset = DataLoader(TestDataset(test_dir, min_size=min_size), batch_size=1)
 
-    G_model = Model(epoch, name=name, gpus=gpus).model
+    G_model = Model(epoch, name=name, gpus=gpus).model.eval()
 
-    print(G_model)
+    # print(G_model)
     for root, dirs, files in os.walk(test_dir):
         imagesNames = files
         break
 
-    test_input = Variable()
+    test_input = Variable(requires_grad = False)
     if (len(gpus) > 0) & torch.cuda.is_available():
         test_input = Variable().cuda()
 
     for index, test_img in enumerate(testDataset):
+        # batchsize, channel, row, col = test_img.size()
+        # test_img = torch.nn.Upsample(scale_factor=1)(test_img)
         batchsize, channel, row, col = test_img.size()
         test_input.data.resize_(test_img.size()).copy_(test_img)  # test input data
-        # if index == 0:
-        #     writer.add_graph(G_model, test_input)
-        #     writer.close()
-        y = G_model(test_input).cpu()
-        y = transforms.ToPILImage()(y.reshape(1, row, col))
+
+        with torch.no_grad():
+            y = G_model(test_input).cpu().reshape(1, row, col)
+
+        y = transforms.Normalize([mean, mean, mean], [std, std, std])(y)  # (-1,1)=>(0,1)   mean = -1,std = 2
+
+        y = transforms.ToPILImage()(y)
+
         y.save("%s/%s_Mask.png" % (result_dir, imagesNames[index][:-4]))
         print("%s/%s_Mask.png finished" % (result_dir, imagesNames[index][:-4]))
         # plt.imshow(y, cmap="gray")
